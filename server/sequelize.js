599ae0d4b14913e42dc3e9683e86f519faed4315
Logan Willis <loganw0315@gmail.com>
2022-01-20 12:54:37 -0600

    Small fix

--- server/sequelize.js
+++ server/sequelize.js
@@ -1,7 +1,9 @@
 const {Sequelize} = require('sequelize')
+require('dotenv').config()
+const uri = process.env.DB_URI
 
 const sequelize = new Sequelize(
-    "    postgres://mtyzoaqdjgmfud:9c423e333cdf3b08e75b7bae22164d9d8c06443b8c39ed98d3f3a5534994b17f@ec2-34-227-120-94.compute-1.amazonaws.com:5432/d1k7c7it30k7en",
+    `${uri}`,
     {
         dialect: "postgres",
         dialectOptions:{
